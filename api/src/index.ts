8146e02d71dae7dc2a3fbc8b5e042b7114eca825
Fadhil <fadhilahdwi.a@gmail.com>
2024-10-29 09:35:20 +0700

    setup server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello node')
+import express, { Request, Response } from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  credentials: true
+}));
+
+app.use(compression());
+app.use(bodyParser.json())
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('server running on http://localhost:8080')
+});
